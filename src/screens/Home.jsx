fc5ed164afa4685665e8cc0bf78e7fe68acd0ee3
KrishnaGitHub18 <krishnadeheriya1803@gmail.com>
2024-07-27 19:34:21 +0530

    changes done

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -3,8 +3,25 @@ import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import Card from '../components/Card';
 import Carousel from '../components/Carousel';
+import { useState, useEffect } from 'react';
 
 const Home = () => {
+
+  const [foodItemsDisplay, setFoodItemsDisplay] = useState([]);
+  const [foodCategoryDisplay, setFoodCategoryDisplay] = useState([]);
+
+  const fetchData = async () => {
+    let res = await fetch("http://localhost:5000/api/foodItemsData", {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    const finalData = await res.json();
+    console.log(finalData); 
+  }
+  useEffect(()=>{fetchData()}, []);
+
   return (
     <div>
       <div> <Navbar /> </div>
